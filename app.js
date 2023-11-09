7b9ac8eaa3e6ae5eb341f383853c5e2d629ad376
sai2963 <venkatsai0629@gmail.com>
2023-11-09 06:02:11 -0800

    project almost compled

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs= require('fs')
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, 'frontend'));
+app.set('view engine', 'ejs');
 
-
-app.set('views',path.join(__dirname,'frontend'));
-app.set('view engine','ejs');
-// Serve the files in the 'frontend' directory
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Route for serving the index.html file
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname, 'frontend', 'index'));
+  res.render('index');
 });
 
 app.get('/share', (req, res) => {
-  res.render(path.join(__dirname, 'frontend', 'form'));
-});
-app.get('/confirm', (req, res) => {
-  res.render(path.join(__dirname, 'frontend', 'confirm'));
+  res.render('form');
 });
 
-app.post('/share',function(req,res){
+app.get('/confirm', (req, res) => {
+  res.render('confirm');
+});
+
+app.post('/share', function (req, res) {
   const restaurant = req.body;
-  const filepath=path.join(__dirname,'data','restaurants.json');
-  const fileData=fs.readFileSync(filepath);
-  const storedRestaurants=JSON.parse(fileData);
+  const filepath = path.join(__dirname, 'data', 'restaurants.json');
+  const fileData = fs.readFileSync(filepath);
+  const storedRestaurants = JSON.parse(fileData);
   storedRestaurants.push(restaurant);
-  fs.writeFileSync(filepath,JSON.stringify(storedRestaurants));
+  fs.writeFileSync(filepath, JSON.stringify(storedRestaurants));
   res.redirect('/confirm');
 });
 
 app.get('/restaurant', (req, res) => {
-  const filepath=path.join(__dirname,'data','restaurants.json');
-  const fileData=fs.readFileSync(filepath);
-  const storedRestaurants=JSON.parse(fileData);
-  res.render(path.join(__dirname, 'frontend', 'restaurant'),{X:storedRestaurants.length});
+  const filepath = path.join(__dirname, 'data', 'restaurants.json');
+  const fileData = fs.readFileSync(filepath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render('restaurant', { X: storedRestaurants.length, restaurants: storedRestaurants });
 });
 
-// Start the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
