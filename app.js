694038b4c0d411f9e3559373680d02cbce127fb7
sai2963 <venkatsai0629@gmail.com>
2023-11-09 00:18:17 -0800

    file updated

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const fs= require('fs')
 
 
 
-const app = express();
+
 app.set('views',path.join(__dirname,'frontend'));
 app.set('view engine','ejs');
 // Serve the files in the 'frontend' directory
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Route for serving the index.html file
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
   res.render(path.join(__dirname, 'frontend', 'index'));
 });
+
 app.get('/share', (req, res) => {
   res.render(path.join(__dirname, 'frontend', 'form'));
 });
 app.get('/confirm', (req, res) => {
   res.render(path.join(__dirname, 'frontend', 'confirm'));
 });
-app.post('/share', function(req, res) {
+
+app.post('/share',function(req,res){
   const restaurant = req.body;
-  const filepath = path.join(__dirname, 'data', 'restaurants.json');
-  let fileData = fs.readFileSync(filepath);
-  let storedRestaurants = JSON.parse(fileData);
+  const filepath=path.join(__dirname,'data','restaurants.json');
+  const fileData=fs.readFileSync(filepath);
+  const storedRestaurants=JSON.parse(fileData);
   storedRestaurants.push(restaurant);
-  fs.writeFileSync(filepath, JSON.stringify(storedRestaurants));
+  fs.writeFileSync(filepath,JSON.stringify(storedRestaurants));
   res.redirect('/confirm');
 });
 
 app.get('/restaurant', (req, res) => {
-  res.render(path.join(__dirname, 'frontend', 'restaurant'),{X:2});
+  const filepath=path.join(__dirname,'data','restaurants.json');
+  const fileData=fs.readFileSync(filepath);
+  const storedRestaurants=JSON.parse(fileData);
+  res.render(path.join(__dirname, 'frontend', 'restaurant'),{X:storedRestaurants.length});
 });
 
 // Start the server
